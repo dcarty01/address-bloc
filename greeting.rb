64a71e8c0c596c7767468e4753011c5ed31f4918
Denise Carty <denisecarty@comcast.net>
2015-06-17 06:45:51 -0400

    updated assignment greeting

--- greeting.rb
+++ greeting.rb
@@ -1,7 +1,12 @@
 def greeting
 
-  ARGV.each do |arg|
-    puts "Hey #{arg}!"
+
+  ARGV.each_with_index do |arg, i|
+    if i != 0
+      puts "#{ARGV.first} #{arg}!"
+    end
   end
 
 end
+
+greeting
